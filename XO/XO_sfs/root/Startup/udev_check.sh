ead4b64f7d5a9a7669c59ad334dbffae6e1696a7
mavrothal <mavrothal@yahoo.com>
2011-09-11 09:04:25 +0300

    Kill powerd if the full udev is not installed

--- XO/XO_sfs/root/Startup/udev_check.sh
+++ XO/XO_sfs/root/Startup/udev_check.sh
@@ -1,4 +1,7 @@
 #!/bin/sh
+# Check if we have the full udev
 if [ ! -d /lib/udev/devices ] ; then
-gtkdialog-splash -fontsize large -bg hotpink -icon gtk-dialog-error -close box -timeout 15 -text "Please add the full udev package, version 151+, from the binary compatible distro so keyboard and power management will work properly" &
+	gtkdialog-splash -fontsize large -bg hotpink -icon gtk-dialog-error -close box -timeout 15 -text "Please add the full udev package, version 151+, from the binary compatible distro so keyboard and power management will work properly" &
+	# ...and kill powerd so user can work for more than 3 minutes...
+	killall powerd
 fi
