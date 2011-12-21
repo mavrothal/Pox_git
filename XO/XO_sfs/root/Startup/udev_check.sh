a646ab7cf1b93bf298c7d23be7a9e6786d9070ae
mavrothal <mavrothal@yahoo.com>
2011-12-21 22:07:07 +0200

    Optional support of gamekeys and power management without udev

--- XO/XO_sfs/root/Startup/udev_check.sh
+++ XO/XO_sfs/root/Startup/udev_check.sh
@@ -1,7 +1,14 @@
 #!/bin/sh
 # Check if we have the full udev
 if [ ! -f /lib/udev/collect ] ; then
-	gtkdialog-splash -fontsize large -bg hotpink -icon gtk-dialog-error -close box -timeout 15 -text "Please add the full udev package, version 151+, from the binary compatible distro so keyboard and power management will work properly" &
 	# ...and kill powerd so user can work for more than 3 minutes...
 	killall powerd
+	killall olpc-switchd
+	killall olpc-kbdshim-udev
+	Xdialog  --title "UDEV Missing" --ok-label "Install" --yesno "Please add the full udev package (with extras), version 151+, \nfrom the binary compatible distro, so keyboard and \npower management will work properly. \n\nAlternativelly you can restore most of the functionaluty \ninstalling the included no-udev-powerd.pet"  0 0 
+	Install=$?
+	if [ $Install -eq 0 ]; then
+		petget /usr/local/share/no-udev-powerd.pet
+	fi
 fi
+
