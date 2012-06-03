5902654d114ec9fe1aa09a28dce87a9d1a904f52
mavrothal <mavrothal@yahoo.com>
2012-06-03 15:05:59 +0300

    Work around for swap braking the system after suspend, in kernel 3.x builds

--- XO/XO_sfs/etc/powerd/presuspend.d/merge.sh
+++ XO/XO_sfs/etc/powerd/presuspend.d/merge.sh
@@ -1,12 +1,20 @@
 #!/bin/sh
 
-# check puppy mode
+# check puppy mode and add a sync of the pupysave before suspend
 #. /etc/rc.d/PUPSTATE
-
-# add a sync of the pupysave before suspend
 #if [ $PUPMODE = "13" ] || [ $PUPMODE = "7" ] ; then
 #        snapmergepuppy
 #fi
+
+# workaround for swap and kernel 3.x
+if [ "`uname -r | cut -c 1`" = "3" ] ; then
+	HASSWAP=`cat /proc/swaps | awk 'END {print $1}'`
+	if [ "$HASSWAP" != "" ] ; then
+		echo $HASSWAP > /tmp/oldswap
+		swapoff -a
+	fi
+fi
+
 touch /tmp/sleeping.now
 sync
 sleep 3
