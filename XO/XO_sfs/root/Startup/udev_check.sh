43d4910d935e0d4ba54d79e74c00b58d2752a918
mavrothal <mavrothal@yahoo.com>
2012-12-01 11:03:46 +0200

    Internationalize some scripts with gettext

--- XO/XO_sfs/root/Startup/udev_check.sh
+++ XO/XO_sfs/root/Startup/udev_check.sh
@@ -1,11 +1,13 @@
 #!/bin/sh
+. gettext.sh
+export TEXTDOMAIN=udev_check.sh
 # Check if we have the full udev
 if [ ! -f /lib/udev/collect ] ; then
 	# ...and kill powerd so user can work for more than 3 minutes...
 	killall powerd
 	killall olpc-switchd
 	killall olpc-kbdshim-udev
-	Xdialog  --title "UDEV Missing" --ok-label "Install" --yesno "Please add the full udev package (with extras), version 151+, \nfrom the binary compatible distro, so keyboard and \npower management will work properly. \n\nAlternativelly you can restore most of the functionaluty \ninstalling the included no-udev-powerd.pet"  0 0 
+	Xdialog  --title "$(gettext 'UDEV Missing')" --ok-label "$(gettext 'Install')" --yesno "$(gettext 'Please add the full udev package (with extras), version 151+, \nfrom the binary compatible distro, so keyboard and \npower management will work properly. \n\nAlternativelly you can restore most of the functionaluty \ninstalling the included no-udev-powerd.pet. \nMake sure you install it AFTER you have created a savefile')"  0 0 
 	Install=$?
 	if [ $Install -eq 0 ]; then
 		petget /usr/local/share/no-udev-powerd.pet
